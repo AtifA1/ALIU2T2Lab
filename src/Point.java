069381f791650679585bb717841af21834807cc7
Atif Ali_ <aatif1771@gmail.com_>
2023-09-28 19:27:53 -0400

    Finished Point and it is full functional.

--- src/Point.java
+++ src/Point.java
@@ -2,6 +2,10 @@ public class Point {
     private int x;
     private int y;
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
     public Point(int value) {
         x = value;
         y = value;
@@ -26,8 +30,22 @@ public class Point {
     }
 
     public String coordinate() {
-        return "(x" + "," + "y)";
+        return "(" + x + ", " + y + ")";
     }
 
-
+    public String quadrant() {
+        if (x>0 && y>0) {
+            return "I";
+        } else if (x<0 && y>0) {
+            return "II";
+        } else if (x<0 && y<0) {
+            return "III";
+        } else if (x>0 && y<0) {
+            return "IV";
+        } else if (x== 0 && y==0) {
+            return "origin";
+        } else {
+            return "on an axis";
+        }
+    }
 }
